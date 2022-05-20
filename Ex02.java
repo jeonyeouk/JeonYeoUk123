07c9a5313f935eb85ee0214291b138d8a90d7e3a
jeonyeouk <jyu7698@naver.com>
2022-05-21 02:09:01 +0900

    JeonYeoUk done

---
+++ Ex02.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Ex02 extends JFrame {
+	public Ex02() {
+		this.setSize(500,300);
+		this.setVisible(true);
+		//this.setTitle("첫프레임");
+	}
+	public static void main(String[] args) {
+		Ex02 frame = new Ex02();
+		//Ex02 f1 = new Ex02();
+	}
+}
